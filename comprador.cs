35c57faf2fe7448ad1245d0f089a0bf51fcf43bb
maydau1524 <21040107@itszo.edu.mx>
2023-06-02 18:26:37 -0600

    actualizacion

--- comprador.cs
+++ comprador.cs
@@ -1,60 +1,68 @@
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
-public class Comprador
+public class Buyer
 {
     public decimal Price { get; set; }
     public string Location { get; set; }
+    public string PropertyType { get; set; }
     public decimal SquareMeters { get; set; }
-    public int NumberOfRooms { get; set; }
-    public bool Furnished { get; set; }
-    public bool FreeOfLien { get; set; }
-    public string TypeProperty { get; set; }
     public string PaymentType { get; set; }
-    public string VendorName { get; set; }
-    public int PhoneNumber { get; set; }
-    public string Email { get; set; }
     public string Description { get; set; }
-    public string Photo { get; set; }
+    public bool FreeOfLiens { get; set; }
+    public string PropertyRegistration { get; set; }
 
-    public void BuscarInmuebleVenta()
+    public void SearchProperty()
     {
-        Console.WriteLine("\nBuscar un Inmueble en Venta!");
+        Console.WriteLine("\nSearch for a Property!");
 
-        string filePath = @"C:\Users\aleja\houserentalv.0.1\HouseForSale.json";
+        string filePath = @"C:\Users\aleja\houserentalv.0.1\PropertiesForSale.json";
 
         try
         {
-            // Leer el archivo JSON
-            string jsonData = File.ReadAllText(filePath);
-
-            // Deserializar el JSON en un objeto Comprador
-            var houseforsaleList = JsonConvert.DeserializeObject<List<Vendedor>>(jsonData);
-
-            // Acceder a las propiedades del objeto
-            foreach (var property in houseforsaleList)
+            // Read the JSON file if it exists
+            if (File.Exists(filePath))
             {
-                Console.WriteLine("Datos obtenidos del archivo:");
-                Console.WriteLine($"Precio: {property.Price}");
-                Console.WriteLine($"Ubicación: {property.Location}");
-                Console.WriteLine($"Metros cuadrados: {property.SquareMeters}");
-                Console.WriteLine($"Número de habitaciones: {property.NumberOfRooms}");
-                Console.WriteLine($"Amueblado: {property.Furnished}");
-                Console.WriteLine($"Libre de gravamen: {property.FreeOfLien}");
-                Console.WriteLine($"Tipo de propiedad: {property.TypeProperty}");
-                Console.WriteLine($"Tipo de pago: {property.PaymentType}");
-                Console.WriteLine($"Nombre del vendedor: {property.VendorName}");
-                Console.WriteLine($"Teléfono: {property.PhoneNumber}");
-                Console.WriteLine($"Correo electrónico: {property.Email}");
-                Console.WriteLine($"Descripción: {property.Description}");
-                Console.WriteLine($"Foto: {property.Photo}");
-                Console.WriteLine();
+                // Read the file content
+                string jsonData = File.ReadAllText(filePath);
+
+                // Deserialize the JSON into a list of Buyer objects
+                var propertiesForSaleList = JsonConvert.DeserializeObject<List<Buyer>>(jsonData);
+
+                // Display the properties for sale that match the buyer's criteria
+                Console.WriteLine("Matching Properties for Sale:");
+
+                foreach (var property in propertiesForSaleList)
+                {
+                    if (property.Location.Equals(Location, StringComparison.OrdinalIgnoreCase)
+                        && property.PropertyType.Equals(PropertyType, StringComparison.OrdinalIgnoreCase)
+                        && property.SquareMeters >= SquareMeters
+                        && property.PaymentType.Equals(PaymentType, StringComparison.OrdinalIgnoreCase)
+                        && property.FreeOfLiens == FreeOfLiens)
+                    {
+                        Console.WriteLine($"Price: {property.Price}");
+                        Console.WriteLine($"Location: {property.Location}");
+                        Console.WriteLine($"Property Type: {property.PropertyType}");
+                        Console.WriteLine($"Square Meters: {property.SquareMeters}");
+                        Console.WriteLine($"Payment Type: {property.PaymentType}");
+                        Console.WriteLine($"Description: {property.Description}");
+                        Console.WriteLine($"Free of Liens: {property.FreeOfLiens}");
+                        Console.WriteLine($"Property Registration: {property.PropertyRegistration}");
+                        Console.WriteLine("---------------------------------------");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No properties for sale found.");
             }
         }
         catch (Exception ex)
         {
-            Console.WriteLine("Error al leer el archivo JSON: " + ex.Message);
+            Console.WriteLine("Error searching for properties: " + ex.Message);
         }
     }
 }
+
